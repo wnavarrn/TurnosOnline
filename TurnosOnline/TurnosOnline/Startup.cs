7d6455f3c8401a8b196c3b78f17cc760b004322b
wnavarrn <navarrwal@gmail.com>
2018-03-06 22:23:32 -0300

    Alta de proyecto
    Primer commit

---
+++ TurnosOnline/TurnosOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurnosOnline.Startup))]
+namespace TurnosOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
