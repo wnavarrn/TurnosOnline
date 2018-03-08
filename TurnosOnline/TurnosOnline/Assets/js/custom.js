f8bb9af7d92c58f0d7886b52d216c3566981166d
wnavarrn <navarrwal@gmail.com>
2018-03-08 17:57:23 -0300

    armando index

--- TurnosOnline/TurnosOnline/Assets/js/custom.js
+++ TurnosOnline/TurnosOnline/Assets/js/custom.js
@@ -1,3 +1,4 @@
+
 /*
 Template Name: Elitehospital Admin
 Author: Themedesigner
---
+++ TurnosOnline/TurnosOnline/Scripts/Home/index.js
@@ -0,0 +1,14 @@
+﻿// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("ready!");
+});
+
+$('#btnModalRegister').click(function () {
+    alert("abri");
+    $("#modalRegister").modal({
+        "backdrop": "static",
+        "keyboard": true,
+        "show": true
+    });
+});
+//modalRegister
